9dbeb42ea4204e656a57ae1707a24d089b04ceeb
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2024-02-18 21:02:50 -0400

    Adicionado resposta do jogo usando innethtml

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
 let btnSalvar = document.getElementById("form__submit");
+let respostaSistema = document.getElementById("resposta__sistema");
 
 const numeroMaximo = 100;
 let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 
 let tentativas = 0;
+let mensagem = "";
 
 function verificandoResultado(event) {
   event.preventDefault();
 
-  let chute = document.getElementById("form__input").value;
+  let chute = parseInt(document.getElementById("form__input").value);
 
-  tentativas++;
   if (numeroSecreto == chute) {
-    alert(
-      `Acertou! Você descobriu o número secreto, o ${numeroSecreto}, ${
-        tentativas == 1 ? "de primeira!" : "em " + tentativas + " tenativas :)"
-      }`
-    );
+    mensagem = `Acertou! Você descobriu o número secreto, o ${numeroSecreto}, ${
+      tentativas == 1 ? "de primeira!" : "em " + tentativas + " tenativas :)"
+    }`;
   } else {
     if (numeroSecreto > chute) {
-      alert(`Tente um número maior que ${chute}.`);
+      mensagem = `Tente um número maior que ${chute}.`;
     } else {
-      alert(`Tente um número menor que ${chute}.`);
+      mensagem = `Tente um número menor que ${chute}.`;
     }
   }
+
+  respostaSistema.innerText = mensagem;
+  tentativas++;
 }
 
 btnSalvar.addEventListener("click", verificandoResultado);
